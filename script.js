0e3f6f03941da07bdbec692354192cee4c4a0605
danirocho <daniel.zambrana@itscordoba.edu.ar>
2025-06-11 00:57:26 -0300

    cuarto commit correción del nav

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const bloqueotrotema = document.getElementById("bloqueotrotema");
 bloquetema.addEventListener("change", function() {
     const op = this.value;
     if (op === "otro") {
-        bloquetema.classList.remove("escondido");
+        bloqueotrotema.classList.remove("escondido");
     } else {
         bloqueotrotema.classList.add("escondido");
     }
